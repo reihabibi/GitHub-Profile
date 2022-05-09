0d12d436895da05a28088834b337e8e1560c4899
Rei Habibi <91762839+reihabibi@users.noreply.github.com>
2022-05-09 19:12:44 +0200

    Added Public Repos Count

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
 
-    $("#table-search").keypress(function(event) {
+    $("#table-search").keypress(function (event) {
         if (event.which === 13) {
             var userProfile = $("#table-search").val();
             getGitProfile(userProfile);
         }
-        
+
     });
 
 });
-
 
 async function getGitProfile(userProfile) {
 
@@ -20,14 +19,15 @@ async function getGitProfile(userProfile) {
     $("#userImg").attr("src", data.avatar_url)
     $("#userUrl").attr("href", data.html_url)
     $("#userName").text("@" + data.login);
-    $("#fullName").text(data.name);    
+    $("#fullName").text(data.name);
     $("#userBio").text(data.bio)
 
     $("#userFollowers").text(" " + data.followers + " Followers")
     $("#userFollowing").text(" " + data.following + " Following")
+    $("#allRepos").text(data.public_repos + " Public Repos")
 
-    $("#allRepos").attr("href", "https://github.com/" + userProfile + "?tab=repositories" )
-    
+    /* $("#allRepos").attr("href", "https://github.com/" + userProfile + "?tab=repositories") */
+
 
     const reposUrl = "https://api.github.com/users/" + userProfile + "/repos#"
     const reposResponde = await fetch(reposUrl);
@@ -36,7 +36,7 @@ async function getGitProfile(userProfile) {
 
     var statusHTML = "";
 
-    $.each(reposData , function(i, status){
+    $.each(reposData, function (i, status) {
 
         statusHTML += '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">';
         statusHTML += '<th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">' + status.name + '</th>';
@@ -45,16 +45,14 @@ async function getGitProfile(userProfile) {
         statusHTML += '<td class="px-6 py-4">' + status.watchers + '</td>';
         statusHTML += '<td class="px-6 py-4">' + status.forks + '</td>';
         statusHTML += '<td class="px-6 py-4">' + status.pushed_at + '</td>';
-        statusHTML += '<td class="px-6 py-4 text-right"><a href="' + status.html_url + '" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View More</a></td>';        
-        statusHTML += '</tr>';      
+        statusHTML += '<td class="px-6 py-4 text-right"><a href="' + status.html_url + '" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View More</a></td>';
+        statusHTML += '</tr>';
 
     })
-
     $('tbody').html(statusHTML);
 
 
     console.log(reposUrl);
     console.log(apiUrl);
     console.log(reposData);
-
 }
